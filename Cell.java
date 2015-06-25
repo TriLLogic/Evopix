1e651d9f4b622f1560ffcbcca264e5ce5fa27365
j3456 <j3456@users.noreply.github.com>
2015-06-25 13:56:03 +0100

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -5,6 +5,7 @@ public class Cell
 	Boolean controlled;
 	Coordinate loc;
 	Type type;
+	int iType;
 
 	//Constructor
 	public Cell(Boolean p, Boolean c, Coordinate l, Type t)
@@ -14,5 +15,16 @@ public class Cell
 		controlled = c;
 		loc = l;
 		type = t;
+		switch(type)
+		{
+		case PHOTOSYNTHESIS: iType = 0; break;
+		case BRAIN: iType = 1; break;
+		case SHELL: iType = 2; break;
+		case FLESH: iType = 3; break;
+		case YELLOW: iType = 4; break;
+		case PURPLE: iType = 5; break;
+		case BLUE: iType = 6; break;
+		case RED: iType = 7; break;
+		}
 	}
 }
