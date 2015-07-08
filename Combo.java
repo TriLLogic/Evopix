435c5b0cebb60843350bcee5c904afbf89b7a92c
j3456 <j3456@users.noreply.github.com>
2015-07-08 13:59:41 +0100

    Update Combo.java

--- Combo.java
+++ Combo.java
@@ -1,34 +1,34 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Combo {
-  
-  private Coordinate[] locations;
-  private ArrayList<Integer> cellTypes;
-    // 1 = yellow
-    // 2 = purple
-    // 3 = blue
-    // 4 = red
-    
-    // Use Coordinates and cell types together for combo
-  
-  
-  // Constructor
-  public Combo(Coordinate[] l) {
-    locations = l;
-    cellTypes = new ArrayList<Integer>();
-    Random rng = new Random();
-    for(int i = 0; i < l.length; i++)
-      cellTypes.set(i, rng.nextInt(4));
-  }
-  
-  // Receive locations
-  public Coordinate[] getLocations() {
-    return locations;
-  }
-  
-  // Used to access cell types
-  public ArrayList<Integer> getCellTypes() {
-    return cellTypes;
-  }
+public class Combo
+{
+	private Coordinate[] locations;
+	private ArrayList<Integer> cellTypes;
+	// 1 = yellow
+	// 2 = purple
+	// 3 = blue
+	// 4 = red
+
+	// Use Coordinates and cell types together for combo
+
+
+	// Constructor
+	public Combo(Coordinate[] l) {
+		locations = l;
+		cellTypes = new ArrayList<Integer>();
+		Random rng = new Random();
+		for(int i = 0; i < l.length; i++)
+			cellTypes.set(i, rng.nextInt(4));
+	}
+
+	// Receive locations
+	public Coordinate[] getLocations() {
+		return locations;
+	}
+
+	// Used to access cell types
+	public ArrayList<Integer> getCellTypes() {
+		return cellTypes;
+	}
 }
